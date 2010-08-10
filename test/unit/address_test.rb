0c66e50b9a2a2b7d60545c3daff3f3e839081294
Matt Hawkins <mhawkins@alextom.com>
2010-08-10 14:45:33 -0400

    added email validation

--- test/unit/address_test.rb
+++ test/unit/address_test.rb
@@ -10,6 +10,12 @@ class AddressTest < ActiveSupport::TestCase
     should belong_to :client
     should belong_to :address_type
     
+    should validate_presence_of :name
+    should validate_presence_of :address1
+    should validate_presence_of :city
+    should validate_presence_of :state
+    should validate_presence_of :phone
+    
     context "with an email" do 
       context "that is valid" do
         setup do
