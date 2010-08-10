598a1d7c91881b4dc9a555d3320f439805bae365
Matt Hawkins <mhawkins@alextom.com>
2010-08-10 16:42:00 -0400

    reordered where scope is in project duration

--- app/models/project_duration.rb
+++ app/models/project_duration.rb
@@ -1,10 +1,10 @@
 class ProjectDuration < ActiveRecord::Base    
   belongs_to :project
   
-  scope :past, where(["end < ?", Date.today])
-  scope :current, where(["start <= ? AND end >= ?", Date.today, Date.today])
-  
   validates :start, :presence => true
   validates :end, :presence => true
   validates_with DurationValidator
+  
+  scope :past, where(["end < ?", Date.today])
+  scope :current, where(["start <= ? AND end >= ?", Date.today, Date.today])
 end
