df1c5bcb3965a5659bfad6b76e16540753ab733b
Matt Hawkins <mhawkins@alextom.com>
2010-08-11 13:43:34 -0400

    added a state machine that will be used to lock a status update

--- app/models/status_update.rb
+++ app/models/status_update.rb
@@ -1,7 +1,22 @@
 class StatusUpdate < ActiveRecord::Base
+  include ::Transitions
+  include ActiveRecord::Transitions
+  
   belongs_to :user
   belongs_to :project_duration
 
   validates :entry_date, :presence => true, :uniqueness => {:scope => :project_duration_id}
   validates :description, :presence => true
+  
+  state_machine do
+    state :open # first one is initial state
+    state :locked
+
+    event :lock do
+      transitions :to => :locked, :from => [:open]
+    end
+    event :reopen do
+      transitions :to => :open, :from => [:locked]
+    end
+  end
 end
--- test/unit/status_update_test.rb
+++ test/unit/status_update_test.rb
@@ -12,6 +12,33 @@ class StatusUpdateTest < ActiveSupport::TestCase
     should validate_presence_of :entry_date
     should validate_uniqueness_of(:entry_date).scoped_to(:project_duration_id)
     should validate_presence_of :description    
+    
+    context "by default" do
+      should "have a status of open" do
+        assert_equal "open", @status_update.state
+      end
+    end
+    
+    context "on lock" do
+      setup do
+        @status_update.lock!
+      end
+      
+      should "have a status of locked" do
+        assert_equal "locked", @status_update.state
+      end
+    end
+    
+    context "that is locked and is reopened" do
+      setup do
+        @status_update.state = "locked"
+        @status_update.reopen!
+      end
+      
+      should "have a status of open" do
+        assert_equal "open", @status_update.state
+      end
+    end
   end
   
 end
