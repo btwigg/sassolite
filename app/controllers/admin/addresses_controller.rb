f0ecac1fec25bde58b09e98781d141aa0f30a602
Matt Hawkins <mhawkins@alextom.com>
2010-08-12 16:11:21 -0400

    added the ability to edit addresses for a client

--- app/controllers/admin/addresses_controller.rb
+++ app/controllers/admin/addresses_controller.rb
@@ -1,2 +1,48 @@
 class Admin::AddressesController < ApplicationController
+  before_filter :load_address_variables
+  
+  def new
+    if @address
+      flash[:error] = "An address with type '#{@address_type.name}' already exists for client '#{@client.name}'."
+      redirect_to admin_clients_path
+    else
+      @address = Address.new(:client => @client, :address_type => @address_type)
+    end
+  end
+  
+  def create
+    @address = Address.new(params[:address])
+    @address.client = @client
+    @address.address_type = @address_type
+    
+    if @address.save
+      flash[:notice] = "Created address of type '#{@address_type.name}' for client '#{@client.name}'."
+      redirect_to admin_clients_path
+    else
+      flash[:error] = "Cannot create address of type '#{@address_type.name}' for client '#{@client.name}'."
+      render :new, :status => :unprocessable_entity
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @address.update_attributes(params[:address])
+      flash[:notice] = ""
+      redirect_to admin_clients_path
+    else
+      flash[:error] = ""
+      render :edit, :status => :unprocessable_entity
+    end
+  end
+  
+  protected
+  
+  def load_address_variables
+    @client = Client.find(params[:client_id])
+    @address_type = AddressType.find(params[:address_type_id])
+    @address = Address.where(:client_id => @client, :address_type_id => @address_type).first
+  end  
+  
 end
--- test/functional/admin/addresses_controller_test.rb
+++ test/functional/admin/addresses_controller_test.rb
@@ -3,8 +3,110 @@ require 'test_helper'
 class Admin::AddressesControllerTest < ActionController::TestCase
   context "An Admin::AddressesController" do
     setup do
-      login_user
-      @address = Factory.create(:address)
+      login_user      
+    end
+    
+    context "for a new address" do
+      setup do
+        @client = Factory.create(:client)
+        @address_type = Factory.create(:address_type)
+      end
+      
+      context "on GET to #new" do
+        setup do
+          get :new, :client_id => @client, :address_type_id => @address_type
+        end
+        
+        should assign_to(:client)
+        should assign_to(:address_type)
+        should assign_to(:address)
+        
+        should_display_a_form
+        should_display_a_breadcrumb
+        
+        should "display the address type" do
+          assert_select "p", /Mailing/
+        end
+      end
+      
+      context "on POST to #create" do
+        
+        context "with valid data" do
+          setup do
+            post :create, :address => Factory.attributes_for(:address), :client_id => @client, :address_type_id => @address_type
+          end
+          
+          should assign_to(:client)
+          should assign_to(:address_type)
+          should assign_to(:address)
+          should set_the_flash.to("Created address of type 'Mailing' for client 'Quentin Corp'.")
+          should redirect_to("admin clients path") { admin_clients_path}
+        end
+        
+        context "with invalid data" do
+          setup do
+            post :create, :address => {}, :client_id => @client, :address_type_id => @address_type
+          end
+          
+          should assign_to(:client)
+          should assign_to(:address_type)
+          should assign_to(:address)
+          should respond_with :unprocessable_entity
+          should set_the_flash.to("Cannot create address of type 'Mailing' for client 'Quentin Corp'.")
+        end
+        
+      end
+      
+    end
+    
+    context "for an existing address" do
+      setup do
+        @address = Factory.create(:address)
+        @address_type = @address.address_type
+        @client = @address.client
+      end
+      
+      context "on GET to #new" do
+        setup do
+          get :new, :client_id => @client, :address_type_id => @address_type, :id => @address
+        end
+        
+        should redirect_to("admin clients path") { admin_clients_path}
+        should set_the_flash.to("An address with type 'Mailing' already exists for client 'Quentin Corp'.")
+      end
+      
+      context "on GET to #edit" do
+        setup do
+          get :edit, :client_id => @client, :address_type_id => @address_type, :id => @address
+        end
+        
+        should assign_to(:client)
+        should assign_to(:address_type)
+        should assign_to(:address)
+        
+        should_display_a_form
+        should_display_a_breadcrumb
+        
+        should "display the address type" do
+          assert_select "p", /Mailing/
+        end
+      end
+      
+      context "on PUT to #update" do
+        
+        context "with valid data" do
+          setup do
+            post :update, :client_id => @client, :address_type_id => @address_type, :id => @address
+          end
+        end
+        
+        context "with invalid data" do
+          setup do
+            post :update, :client_id => @client, :address_type_id => @address_type, :id => @address
+          end
+        end
+        
+      end
     end
     
     
