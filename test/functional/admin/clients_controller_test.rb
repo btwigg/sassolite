c5ce69dfde8600e718ba5a18095f88cc536968c1
Matt Hawkins <mhawkins@alextom.com>
2010-08-12 12:49:12 -0400

    flushed out controller test for admin/clients

--- test/functional/admin/clients_controller_test.rb
+++ test/functional/admin/clients_controller_test.rb
@@ -9,7 +9,7 @@ class Admin::ClientsControllerTest < ActionController::TestCase
     
     context "on GET to #index" do
       setup do
-        get :index
+        get :index, :id => @client.name
       end
       
       should respond_with :success
@@ -43,44 +43,90 @@ class Admin::ClientsControllerTest < ActionController::TestCase
         assert_select "form"
       end
     end
-    
+       
     context "on POST to #create" do
+           
+      context "with valid data" do
+        setup do
+          post :create, :client => { :name => "New Client X"}
+        end
+  
+        should redirect_to("admin clients index") { admin_clients_path }
+        should assign_to(:client)
+      end
+
+      context "with invalid data" do
+        setup do
+          post :create, :client => {}
+        end
+  
+        should respond_with :unprocessable_entity
+        should assign_to :client
+        should render_template :new
+  
+        should "display an error message" do
+          assert_select "div.errorExplanation"
+        end
+      end
+
+    end
+
+    context "on GET to #edit" do
+      setup do
+        get :edit, :id => @client
+      end
+  
+      should respond_with :success
+      should assign_to :client
+      should render_template :edit
+  
+      should "display Editing 'Client Name' headline" do
+        assert_select "h2", /Editing '#{@client.name}'/
+      end
+  
+      should "display a form" do
+        assert_select "form"
+      end
+    end
+    
+    context "on PUT to #update" do
       
       context "with valid data" do
         setup do
-          post :create
+          put :update, :client => { :name => "New Client"}, :id => @client
         end
         
         should redirect_to("admin clients index") { admin_clients_path }
-        should assign_to(:client)
-        
+        should assign_to :client
+        should set_the_flash.to "Client 'Quentin Corp' has been updated."
       end
       
       context "with invalid data" do
         setup do
-          post :create
+          put :update, :client => { :name => ""}
         end
         
-        should respond_with :success
+        should respond_with :unprocessable_entity
         should assign_to :client
-        should render_template :new
-        
+        should render_template :edit
+        should set_the_flash.to "Client could not be updated."
+
         should "display an error message" do
           assert_select "div.errorExplanation"
         end
       end
       
     end
-    
+       
     context "on DELETE to #destroy" do
       setup do
         delete :destroy, :id => @client
       end
-      
+  
       should redirect_to("admin clients index") { admin_clients_path }
       should assign_to(:client)
-      should set_the_flash.to "Client 'Quentin Corp' has been deleted"
-      
+      should set_the_flash.to "Client 'Quentin Corp' has been deleted."
+  
     end
   end
 end
