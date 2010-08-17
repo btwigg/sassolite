8eea905016cc6cd61904b91ae6d8638967b9e6da
Sam Goldman <sam.goldman@alextom.com>
2010-08-17 10:39:52 -0400

    Admin project durations model/view/controllers.

--- app/controllers/admin/project_durations_controller.rb
+++ app/controllers/admin/project_durations_controller.rb
@@ -1,3 +1,45 @@
 class Admin::ProjectDurationsController < ApplicationController
+  before_filter :load_relational_data
+  
+  def index
+    @project_durations = @project.project_durations
+  end
+  
+  def new
+    @project_duration = ProjectDuration.new
+  end
+  
+  def create
+    @project_duration = @project.project_durations.new(params[:project_duration])
+  
+    if @project_duration.save
+      flash[:notice] = "New duration for project \"#{@project.name}\" has been created."
+      redirect_to admin_project_project_durations_path(@project)
+    else
+      flash[:error] = "Project duration could not be created."
+      render :new, :status => :unprocessable_entity
+    end
+  end
+  
+  def edit
+    @project_duration = ProjectDuration.find(params[:id])
+  end
+  
+  def update
+    redirect_to admin_project_project_durations_path(@project)
+  end
+  
+  def destroy
+    @project_duration = ProjectDuration.find(params[:id])
+    @project_duration.destroy
+    flash[:notice] = "Project #{@project.name} #{@project_duration.name} has been deleted."
+    redirect_to admin_project_project_durations_path
+  end
+  
+  private
+  
+  def load_relational_data
+    @project = Project.find(params[:project_id])
+  end
   
 end
